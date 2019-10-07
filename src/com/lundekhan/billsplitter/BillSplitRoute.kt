e117f89c06cbb4644651f9480bc48cfd2062b141
Lundez <hampus.londogard@gmail.com>
2019-10-07 19:50:01 +0200

    BillSplitWIP

--- src/com/lundekhan/billsplitter/BillSplitRoute.kt
+++ src/com/lundekhan/billsplitter/BillSplitRoute.kt
@@ -2,25 +2,44 @@ package com.lundekhan.billsplitter
 
 import com.lundekhan.htmltemplates.respondHtmlDefault
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveParameters
+import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
-import kotlinx.html.hardTextArea
-import kotlinx.html.p
-import kotlinx.html.submitInput
+import kotlinx.html.*
 
 fun Route.BillSplit() {
     route("/billsplit") {
         get {
             call.respondHtmlDefault("billsplit.", 4) {
                 p { + "Bill Splitter. Format: <NAME> <AMOUNT>" }
-                p { + "Split by spaces, comma or newline." }
-                hardTextArea { }
-                submitInput {  }
+                p { + "Split by comma" }
+                form(encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
+                    input {
+                        type = InputType.text
+                        name = "text"
+                    }
+                    br
+                    input {
+                        type = InputType.submit
+                        value = "submit"
+                    }
+                }
             }
         }
-        post {
+        post("/billsplit") {
+            call
+                .receiveParameters()["text"]
+                ?.split(',')
+                ?.map { personAmount ->
+                    personAmount
+                        .split(' ')
+                        .take(2)
+                        }
+            val text = call.receiveParameters()["text"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Persons & amount must be supplied (<NAME> <AMOUNT>, ...).")
 
         }
     }
