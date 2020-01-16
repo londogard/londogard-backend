bbc98bbee52d7d1b8c6b33e82e6d1e6d236a70ab
Lundez <hampus.londogard@gmail.com>
2020-01-16 19:23:08 +0100

    Moved val to const, compile-time const

--- src/com/lundekhan/CustomExceptions.kt
+++ src/com/lundekhan/CustomExceptions.kt
@@ -4,4 +4,4 @@ class InvalidCredentialsException(message: String) : RuntimeException(message)
 class InvalidInputException(message: String) : RuntimeException(message)
 class UserCreationException(message: String = "User could not be created") : RuntimeException(message)
 class InvalidRouteException(message: String = "Route does not exist") : RuntimeException(message)
-val unknownError = "UNKNOWN ERROR"
+const val unknownError = "UNKNOWN ERROR"
