940c0468e7b2b41816660f3950c2a5a5d0cb846f
Hampus Londögård <hampus.londogard@gmail.com>
2020-01-16 19:29:46 +0100

    Text gen model routes (#5)
    * Added path to update models, set temp & token count

--- src/com/lundekhan/Responses.kt
+++ src/com/lundekhan/Responses.kt
@@ -3,3 +3,4 @@ package com.lundekhan
 fun resultResponse(output: String): Map<String, Any> = mapOf("result" to output)
 
 data class ResultResponse(val result: String)
+data class ResultResponseArray(val result: List<String>)
--- src/com/lundekhan/textgen/TextgenRoute.kt
+++ src/com/lundekhan/textgen/TextgenRoute.kt
@@ -1,11 +1,15 @@
 package com.lundekhan.textgen
 
 import com.londogard.textgen.LanguageModel
+import com.londogard.textgen.PretrainedModels
+import com.lundekhan.InvalidRouteException
+import com.lundekhan.ResultResponseArray
 import com.lundekhan.resultResponse
 import io.ktor.application.call
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
 import kotlinx.coroutines.Dispatchers
@@ -16,6 +20,13 @@ import org.koin.ktor.ext.inject
 @ImplicitReflectionSerializer
 fun Route.textgenRoute(): Route = route("/textgen") {
     val languageModel by inject<LanguageModel>()
+    var currentModel: String = PretrainedModels.SHAKESPEARE.name
+    val models by lazy {
+        PretrainedModels
+            .values()
+            .filterNot { it == PretrainedModels.CUSTOM }
+            .map { it.name }
+    }
 
     post {
         val seedText = call.receive<TextGenInput>().text
@@ -25,6 +36,29 @@ fun Route.textgenRoute(): Route = route("/textgen") {
 
         call.respond(resultResponse(genText))
     }
+
+    post("/{model}") {
+        val seed = call.receive<TextGenInput>()
+        val modelName = call.parameters["model"]
+
+        if (currentModel != modelName && modelName != null) {
+            currentModel = modelName
+            val model = PretrainedModels
+                .values()
+                .find {  it.name == modelName } ?: throw InvalidRouteException("Model '${call.parameters["model"]}' does not exist.")
+            languageModel.changeModelToPretrained(model)
+        }
+
+        val generatedText = withContext(Dispatchers.Default) {
+            languageModel.generateText(seed.text, seed.tokens, seed.temperature)
+        }
+
+        call.respond(resultResponse(generatedText))
+    }
+
+    get("/models") {
+        call.respond(ResultResponseArray(models))
+    }
 }
 
-data class TextGenInput(val text: String)
+data class TextGenInput(val text: String, val temperature: Double = 0.3, val tokens: Int = 150)
