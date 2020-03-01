d3024c0ab1d6a98bcdaf665fe4d9e4f0aaccf7d7
Lundez <hampus.londogard@gmail.com>
2020-03-01 16:50:02 +0100

    route blog/update added w/ DB integration

--- src/com/lundekhan/blog/BlogRoute.kt
+++ src/com/lundekhan/blog/BlogRoute.kt
@@ -2,9 +2,11 @@ package com.lundekhan.blog
 
 import com.lundekhan.Database
 import com.lundekhan.InvalidRouteException
+import com.lundekhan.data.Blog
 import com.lundekhan.resultResponse
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -38,6 +40,18 @@ fun Route.blogRoute(): Route = route("/blog") {
             ?: throw InvalidRouteException("Blog $id does not exist")
     }
     authenticate {
+        post("/update") {
+            val blog = call.receive<BlogPostOpt>()
+            val oldBlog = db.blogQueries.selectById(blog.id).executeAsOneOrNull() as Blog.Impl? ?: return@post call.respond(HttpStatusCode.NotFound)
+            val updatedBlog = oldBlog.copy(
+                title = blog.title ?: oldBlog.title,
+                summary = blog.summary ?: oldBlog.summary,
+                blog_body = blog.blogBody ?: oldBlog.blog_body,
+                category = blog.category ?: oldBlog.category
+                )
+            db.blogQueries.insertOrReplace(updatedBlog)
+            call.respond(resultResponse("Blog successfully updated"))
+        }
         post("/create") {
             val blog = call.receive<BlogPost>()
             db.blogQueries.insert(blog.title, blog.summary, LocalDateTime.now(), blog.blogBody, blog.category) // We catch exception in StatusPage
@@ -46,5 +60,6 @@ fun Route.blogRoute(): Route = route("/blog") {
     }
 }
 
+data class BlogPostOpt(val id: Long, val title: String?, val summary: String?, val blogBody: String?, val category: List<String>?)
 data class BlogPost(val title: String, val summary: String, val blogBody: String, val category: List<String>)
 data class FullBlog(val title: String, val summary: String, val blogBody: String, val category: List<String>, val date: String, val id: Long)
